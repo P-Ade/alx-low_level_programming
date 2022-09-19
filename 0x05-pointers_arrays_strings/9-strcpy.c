21977fd7fabd6efb728fecbc3feb94c47e6d1478
Promise Adebayo <adebayopromise36@gmail.com>
2022-09-19 15:55:49 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,6 +3,7 @@
 /**
 * _strcpy - Copy paste string
 * @dest: destination
+* @src: source
 *
 * Return: destination
 */
@@ -11,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int inc = 0;
 
-	while (*(src + inc) = *(src + inc);
+	while (*(src + inc) != '\0')
 	{
 		*(dest + inc) = *(src + inc);
 		inc++;
