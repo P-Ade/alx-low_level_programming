7744899a55be357c24abdc1fd463ddc57cfcedbb
Promise Adebayo <adebayopromise36@gmail.com>
2022-09-19 15:45:38 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcpy - Copy paste string
+* @dest: destination
+*
+* Return: destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int inc = 0;
+
+	while (*(src + inc) = *(src + inc);
+	{
+		*(dest + inc) = *(src + inc);
+		inc++;
+	}
+	*(dest + inc) = '\0';
+
+	return (dest);
+}
