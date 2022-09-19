28ee0a37c5a06a9ee3e4cdb28dab5bf3f87053b0
Promise Adebayo <adebayopromise36@gmail.com>
2022-09-19 15:29:43 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,11 @@
 * Return: nothing
 */
 
-
 void print_array(int *a, int n)
 {
 	int inc;
 
-	for (inc = a; inc < n ; inc++)
+	for (inc = 0; inc < n; inc++)
 	{
 		if (inc != n - 1)
 			printf("%d, ", a[inc]);
