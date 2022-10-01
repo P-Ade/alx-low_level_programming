158154c99e3884de6301a19c3894f659e108fe20
Promise Adebayo <adebayopromise36@gmail.com>
2022-09-30 17:39:09 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - program that prints its name, followed by a new line
- * @argc: argument that counts argument input
+ * @argc: argument that contains argument input
  * @argv: argument that stores the strings in an array of char* (strings)
  * Return: 0
  */
