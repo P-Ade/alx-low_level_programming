3f7bbdff2b9ade616d98889cbce64cb1cc45a124
Promise Adebayo <adebayopromise36@gmail.com>
2022-10-03 18:04:45 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* create_array - creates an array of chars, and
-* initializes it with a specific char.
+*create_array - creates an array of chars, and
+*initializes it with a specific char.
 *@size: Size of the array
 *@c: Character to insert
 *Return: NULL if size is zero or if it fails,
@@ -21,7 +21,7 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL);
 
-	for (index =0; index < size; index++)
+	for (index = 0; index < size; index++)
 		array[index] = c;
 
 	return (array);
